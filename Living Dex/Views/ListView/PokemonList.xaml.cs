e1136bf0d19d773c031e1c99016de33745e76b0c
Abbie Gorlov-Webster <AbbiegorlovWebster@gmail.com>
2024-09-17 00:29:06 +0100

    List View
    List View initial commit

---
+++ Living Dex/Views/ListView/PokemonList.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Living_Dex.Views.ListView
+{
+    /// <summary>
+    /// Interaction logic for PokemonList.xaml
+    /// </summary>
+    public partial class PokemonList : UserControl
+    {
+        public PokemonList()
+        {
+            InitializeComponent();
+        }
+    }
+}
